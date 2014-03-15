0bade354508122fdcc976a5239ded8ab93783eb6
Shawn South <shawn.south@bellevuecollege.edu>
2014-03-14 19:19:13 -0700

    Added auto-fill mappings configuration example

--- gravityforms-external-data-fields/EXAMPLE-gravityforms-external-data-fields-config.php
+++ gravityforms-external-data-fields/EXAMPLE-gravityforms-external-data-fields-config.php
@@ -1,1 +1,14 @@
 <?php
+// TODO: make a copy of this file w/ the 'EXAMPLE-' prefix removed, then uncomment the following lines & add your settings
+
+//class autoFillSettings
+//{
+//  // Connection string
+//  public $connection = "";
+//  // Table name, stored procedure or SELECT statement
+//  public $command = "";
+//  // List of mappings - from the database column name to the form field
+//  public $mappings = array(
+//    "fieldID" => "columnName"
+//  );
+//}
