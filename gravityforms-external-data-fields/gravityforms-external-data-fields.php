1e9f840e4a56c7635c1570299a73b667b1e45cfe
Erik Steen <erik.steen@bellevuecollege.edu>
2014-03-17 13:10:25 -0700

    Add header info so WordPress recognizes our file as a plugin

--- gravityforms-external-data-fields/gravityforms-external-data-fields.php
+++ gravityforms-external-data-fields/gravityforms-external-data-fields.php
@@ -1,2 +1,11 @@
 <?php
+/*
+Plugin Name: Bellevue College Authenticated Forms
+Plugin URI: http://www.bellevuecollege.edu
+Description: Extend Gravity Forms with Bellevue College form field data
+Author: Bellevue College Technology Development and Communications
+Version: 0.1
+Author URI: http://www.bellevuecollege.edu
+*/
+
 require_once("gravityforms-external-data-fields-config.php");
