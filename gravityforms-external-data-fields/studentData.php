ef52ef87efa722856ec073168311459cd74bb0c2
Shawn South <shawn.south@bellevuecollege.edu>
2014-03-18 18:12:54 -0700

    Successfully queries database.
    One unit test (out of 2) passes.

--- gravityforms-external-data-fields/studentData.php
+++ gravityforms-external-data-fields/studentData.php
@@ -18,36 +18,56 @@ class studentData
   {
     $this->studentID = $sid;
 
+    // temporary test data
+//    $this->firstName = "Shawn";
+//    $this->lastName = "South";
+//    $this->emailAddress = "shawn.south@bellevuecollege.edu";
+
     // retrieve student info from database
     try
     {
-      $dbh = new PDO(gf_external_data_fields_config::$dsn, gf_external_data_fields_config::$studentDataLogin, gf_external_data_fields_config::$studentDataPassword);
+      $dbh = new PDO(gf_external_data_fields_config::$dsn);//,
+//                     gf_external_data_fields_config::$studentDataLogin,
+//                     gf_external_data_fields_config::$studentDataPassword);
 
-      $rs = $dbh->query(gf_external_data_fields_config::$studentQuery);
-      // TODO: use fetch() - http://docs.php.net/manual/en/pdostatement.fetch.php
+//      $q = $dbh->query(gf_external_data_fields_config::$studentQuery);
 
-      if(($rs) && ($rs->rowCount() > 0))
+      $query = $dbh->prepare(gf_external_data_fields_config::$studentQuery);
+      $query->execute();
+
+      if($query)
       {
-        if($rs->rowCount() > 1)
+        $rs = $query->fetch(PDO::FETCH_ASSOC);
+
+        if($rs)
         {
-          // TODO: warn that query returned too many records
+          $this->firstName = $rs[gf_external_data_fields_config::$sqlColumnFirstName];
+          $this->lastName = $rs[gf_external_data_fields_config::$sqlColumnLastName];
+          $this->emailAddress = $rs[gf_external_data_fields_config::$sqlColumnEmailAddress];
+        }
+        else
+        {
+          print("Student record is null!");
+          print_r($query->errorInfo());
+//          die("Student record is null!");
         }
       }
       else
       {
-        die("Student query returned no records.");
+        print("Query results are null!");
+        print_r($query->errorInfo());
+//        die("Student query returned no records.");
       }
     }
     catch(PDOException $ex)
     {
       // TODO: log error message
-      die($ex->getMessage());
+      print_r($ex->getMessage());
+//      die($ex->getMessage());
     }
 
-    // temporary test data
-//    $this->firstName = "Shawn";
-//    $this->lastName = "South";
-//    $this->emailAddress = "shawn.south@bellevuecollege.edu";
+    // close database connection
+    $dbh = null;
   }
 
   /**
