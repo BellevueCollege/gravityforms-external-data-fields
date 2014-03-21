570572025493c5991b2cfb428cc8938f8051115a
Shawn South <shawn.south@bellevuecollege.edu>
2014-03-21 10:24:23 -0700

    Updated config example

--- gravityforms-external-data-fields/EXAMPLE-gravityforms-external-data-fields-config.php
+++ gravityforms-external-data-fields/EXAMPLE-gravityforms-external-data-fields-config.php
@@ -1,31 +1,32 @@
 <?php
 // TODO: make a copy of this file w/ the 'EXAMPLE-' prefix removed, then uncomment the following lines & add your settings
 
-//$file_upload_path = "/var/www/files/"; // Set this to physical path of the directory
-//$file_upload_url = "http://example.com/files/"; // Set this to the url of the directory
+/* NOTE: commenting just this line will expose the code
 
-/*
+$file_upload_path = "/var/www/files/"; // Set this to physical path of the directory
+$file_upload_url = "http://example.com/files/"; // Set this to the url of the directory
+
 class gf_external_data_fields_config
 {
-  // See http://docs.php.net/manual/en/ref.pdo-odbc.connection.php
+  // See http://docs.php.net/manual/en/ref.pdo-dblib.connection.php
   public static $dsn = "";
   // Database credentials
+  // NOTE: Only SQL accounts if connecting to MSSQL (AD accounts do not work)
   public $studentDataLogin = "";
   public $studentDataPassword = "";
 
-  public static $studentQuery = "";
-}
-//*/
+  public static $studentQuery = <<<EOS
+SELECT statement goes here
+EOS;
 
-//class autoFillSettings
-//{
-//  // Connection string
-//  public $connection = "";
-//  // Table name, stored procedure or SELECT statement
-//  public $command = "";
-//  // List of mappings - from the database column name to the form field
-//  public $mappings = array(
-//    "fieldID" => "columnName"
-//  );
-//}
+  // SQL column names
+  public static $sqlColumnStudentID = "SID";
+  public static $sqlColumnFirstName = "FirstName";
+  public static $sqlColumnLastName = "LastName";
+  public static $sqlColumnEmailAddress = "Email";
+  public static $sqlColumnDaytimePhone = "DaytimePhone";
+  public static $sqlColumnEveningPhone = "EveningPhone";
+}
+
+//*/
 
