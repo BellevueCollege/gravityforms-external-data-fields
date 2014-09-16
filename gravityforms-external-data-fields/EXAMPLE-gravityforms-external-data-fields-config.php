ac2bc6aa5a0aa73a5fe58c3b37e679768047f995
Tripti Sharma <tripti.sharma@bellevuecollege.edu>
2014-09-16 13:39:26 -0700

    Updated the example config file to add the employee query and fields

--- gravityforms-external-data-fields/EXAMPLE-gravityforms-external-data-fields-config.php
+++ gravityforms-external-data-fields/EXAMPLE-gravityforms-external-data-fields-config.php
@@ -28,6 +28,16 @@ class gf_external_data_fields_config
 public static $employeeQuery =  <<<EOS
 SELECT statement
 EOS;
+ 
+    public static $sqlColumnEmployeeSSN = "SSN";
+    public static $sqlColumnEmployeeSID = "SID";
+    public static $sqlColumnEmployeeFirstName = "FirstName";
+    public static $sqlColumnEmployeeLastName = "LastName";
+    public static $sqlColumnEmployeeEmailAddress = "WorkEmail";
+    public static $sqlColumnEmployeeDaytimePhone = "WorkPhoneNumber";
+
+
+
   public $studentDataLogin = "";
   public $studentDataPassword = "";
 
