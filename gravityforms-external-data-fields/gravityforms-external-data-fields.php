7cd792ac53ad8fda8e79e1819261f956eac51af5
builduser01 <borisov_nikita@yahoo.com>
2015-10-14 12:26:39 -0700

    bump version to 1.3.0.1

--- gravityforms-external-data-fields/gravityforms-external-data-fields.php
+++ gravityforms-external-data-fields/gravityforms-external-data-fields.php
@@ -4,7 +4,7 @@ Plugin Name: Gravity Forms External Data Fields
 Plugin URI: https://github.com/BellevueCollege/gravityforms-external-data-fields
 Description: Extend Gravity Forms with Bellevue College form field data
 Author: Bellevue College Technology Development and Communications
-Version: 1.3
+Version: 1.3.0.1
 Author URI: http://www.bellevuecollege.edu
 */
 
